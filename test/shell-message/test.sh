fe37c63346c386ea509580dc63c68e397fbf716d
Mal Curtis <mal@mal.co.nz>
2024-07-03 01:24:39 +0000

    Get tests all sorted

--- test/shell-message/test.sh
+++ test/shell-message/test.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "validate system boots" fastfetch | grep 'tailscale'
+check "validate system boots" fastfetch | grep 'Tailscale'
 
 reportResults
